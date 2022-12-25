a3c537d988d848ac1d885223d5c3bf6f14dda165
影凌 <36478298+KageRyo@users.noreply.github.com>
2022-12-25 15:22:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ from pybit import spot              # 查詢實際交易市場的接口
 import kline
 import discord
 
+"""
+Bybit-Predict 是 CodeRyo 團隊基於 BybitAPI 開發應用於 Discord 上的加密貨幣趨勢機器人，
+開源LICENSE：GNU General Public License v2.0
+Bybit-Predict is a cryptocurrency trend prediction robot developed by the CodeRyo team based on the BybitAPI for use on Discord. 
+open source LICENSE: GNU General Public License v2.0
+"""
+
 def predict(Name): #呼叫各函式進行判斷
     pass
 
