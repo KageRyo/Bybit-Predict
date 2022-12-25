f6036cfb1c42b70f01238aac82743f09bd648533
影凌 <36478298+KageRyo@users.noreply.github.com>
2022-12-25 15:18:27 +0800

    Update kline.py

--- kline.py
+++ kline.py
@@ -1,1 +1,38 @@
-# kline.py
+def KLineStatus(times,Name): #呼叫實盤K線數據
+    pass
+
+def NKLineStatus(Name):
+    pass
+
+def savedata(Kline):#存取K線資料
+    pass
+
+def PB(T): #回推K線
+    pass
+
+def AAA(): #計算每六根K線的平均
+    pass
+
+def Variation(openTime,open,close): #計算時間線
+    pass
+
+def Wtime(): #換算UTC為秒
+    pass
+
+def Ktime(time_stamp): #換算秒為UTC
+    pass
+
+def abc(open,high,low,close): #檢測多空頭
+    pass
+
+def powerUP(volume): #計算多頭量能
+    pass
+
+def powerDOWN(volume): #計算空頭量能
+    pass
+
+def Compare(UP,DOWN): #比較多空權勢
+    pass
+
+def re(EX): #計算點位
+    pass
