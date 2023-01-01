3ddcec6e24fd384cd2672a6b3f8a9f97dad3aa0c
影凌 <36478298+KageRyo@users.noreply.github.com>
2023-01-01 12:53:54 +0800

    Update kline.py

--- kline.py
+++ kline.py
@@ -1,5 +1,12 @@
 import imports as ipk
 
+""" 
+Bybit-Predict 是 CodeRyo 團隊基於 BybitAPI 開發應用於 Discord 上的加密貨幣趨勢機器人，
+開源LICENSE：GNU General Public License v2.0
+Bybit-Predict is a cryptocurrency trend prediction robot developed by the CodeRyo team based on the BybitAPI for use on Discord. 
+open source LICENSE: GNU General Public License v2.0
+"""
+
 bullK = []               # 高於平均的多頭K線
 bearK = []               # 高於平均的空頭K線
 trendType = []           # 紀錄多空頭與十字線
